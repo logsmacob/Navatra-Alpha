0c593c37b444cbf9f8d99e2799043a0c4335e8b2
logsmacob <LMACURDY315@GMAIL.COM>
2026-03-26 05:53:11 -0600

    ljh. n

---
+++ features/ItemButton/item_button_animation.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+
+func _on_item_button_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_item_button_mouse_exited() -> void:
+	pass # Replace with function body.
